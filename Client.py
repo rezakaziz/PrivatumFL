d1c76e5614edf15eeaef93515b0a676d69ba177b
rezakaziz <rezak430@gmail.com>
2023-12-04 14:42:23 +0100

    creating the architecture

---
+++ Client.py
@@ -0,0 +1,9 @@
+# Download the model. 
+# Local Training.
+# Encrypting the updates with paillier scheme and use the public key of the server.
+# Sending data updates to the proxy.
+
+class Client:
+    def __init__(self,dataset):
+        pass
+    
---
+++ Server.py
@@ -0,0 +1,7 @@
+# Receive encrypted updates
+# Decrypt 
+# Do the aggregation step.
+
+class Server:
+    def __init__(self):
+        pass
