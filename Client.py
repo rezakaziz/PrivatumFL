d727ec9f00c24fde9bd7756e94c6fc71f4fb3e1b
rezakaziz <rezak430@gmail.com>
2023-12-11 12:44:17 +0100

    The model is implemented, Need to be tested

--- Client.py
+++ Client.py
@@ -2,8 +2,27 @@
 # Local Training.
 # Encrypting the updates with paillier scheme and use the public key of the server.
 # Sending data updates to the proxy.
+import flwr as fl
+"""
+class Client(fl.client.NumPyClient):
+    def get_parameters(self):
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
-class Client:
-    def __init__(self,dataset):
-        pass
-    
+    def set_parameters(self, parameters):
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(net, trainloader, epochs=1)
+        return self.get_parameters(), num_examples["trainset"], {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(net, testloader)
+        return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
+
+
+fl.client.start_numpy_client("127.0.0.1:8080", client=Client())
+"""
---
+++ Model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super(Net, self).__init__()
+        # 2 Convolution Layers
+        self.conv1 = nn.Conv2d(1, 128, 5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv2 = nn.Conv2d(128, 64, 3)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(64 * 4 * 4, 128) 
+        self.relu = nn.ReLU()
+
+        # Output Layer
+        self.fc2 = nn.Linear(128, 62)
+        self.soft_max = nn.Softmax(1)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool1(self.relu(self.conv1(x)))
+        x = self.pool2(self.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 4 * 4)
+        
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x 
+
+#test and softmax
